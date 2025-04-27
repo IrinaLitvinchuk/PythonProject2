e8f0bf36dd8d1dfea026f12117f879e6d96c7810
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-04-27 22:04:04 +0300

    tests update

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,26 @@
+from mypy.checkexpr import replace_callable_return_type
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая принимает на вход номер карты и возвращает ее маску"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-
-
-# card_number = input("Номер карты: ")
-# print(get_mask_card_number(card_number))
+    if card_number != '':
+        if isinstance(card_number, str):
+            if len(card_number) == 16:
+                return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+            return 'Номер карты должен состоять из 16 цифр'
+        else:
+            raise TypeError
+    return 'Введите номер карты'
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция, которая принимает на вход номер счета и возвращает его маску"""
-    return f"**{account_number[-4:]}"
+    if account_number != '':
+        if isinstance(account_number, str):
+            if len(account_number) == 20:
+                return f"**{account_number[-4:]}"
+            return 'Номер счета должен состоять из 20 цифр'
+        else:
+            raise TypeError
+    return 'Ведите номер счета'
 
-
-# account_number = input("Номер счета: ")
-# print(get_mask_account(account_number))
