b8a01bb7a71ef40dc75a2254424835aa072eae09
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-04-27 22:13:19 +0300

    tests_masks formatted with linters

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
+
 @pytest.fixture
 def card_number():
-    return '7000792289606361'
+    return "7000792289606361"
+
 
 @pytest.fixture
 def account_number():
-    return '73654108430135874305'
+    return "73654108430135874305"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,25 +1,28 @@
 import pytest
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == '7000 79** **** 6361'
+    assert get_mask_card_number(card_number) == "7000 79** **** 6361"
 
 
 def test_get_mask_card_number_empty():
-    assert get_mask_card_number('') == 'Введите номер карты'
+    assert get_mask_card_number("") == "Введите номер карты"
 
 
 def test_get_mask_card_number_wrong_digits():
-    assert get_mask_card_number("12345") == 'Номер карты должен состоять из 16 цифр'
+    assert get_mask_card_number("12345") == "Номер карты должен состоять из 16 цифр"
 
 
 def test_get_mask_card_number_wrong_type():
     with pytest.raises(TypeError):
         get_mask_card_number(12345)
 
+
 def test_get_mask_account(account_number):
-    assert get_mask_account(account_number) == '**4305'
+    assert get_mask_account(account_number) == "**4305"
+
 
 def test_get_mask_account_wrong_type():
     with pytest.raises(TypeError):
@@ -27,8 +30,8 @@ def test_get_mask_account_wrong_type():
 
 
 def test_get_mask_account_wrong_digit_count():
-    assert get_mask_account('12345') == 'Номер счета должен состоять из 20 цифр'
+    assert get_mask_account("12345") == "Номер счета должен состоять из 20 цифр"
 
 
 def test_get_mask_account_empty():
-    assert get_mask_account('') == 'Ведите номер счета'
+    assert get_mask_account("") == "Ведите номер счета"
