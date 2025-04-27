2e07872cc055149494c5c49c9890c1c7d627dd69
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-04-27 21:56:42 +0300

    add tests for mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,23 +1,34 @@
 import pytest
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 def test_get_mask_card_number(card_number):
     assert get_mask_card_number(card_number) == '7000 79** **** 6361'
 
 
 def test_get_mask_card_number_empty():
-    assert get_mask_card_number('') == 'Введите номер карты. Номер карты состоит из 16 цифр'
+    assert get_mask_card_number('') == 'Введите номер карты'
+
 
 def test_get_mask_card_number_wrong_digits():
-    assert get_mask_card_number("12345") == 'Введите номер карты. Номер карты состоит из 16 цифр'
+    assert get_mask_card_number("12345") == 'Номер карты должен состоять из 16 цифр'
 
 
 def test_get_mask_card_number_wrong_type():
-    with pytest.raises (TypeError):
+    with pytest.raises(TypeError):
         get_mask_card_number(12345)
 
+def test_get_mask_account(account_number):
+    assert get_mask_account(account_number) == '**4305'
+
+def test_get_mask_account_wrong_type():
+    with pytest.raises(TypeError):
+        get_mask_account(12345)
 
 
+def test_get_mask_account_wrong_digit_count():
+    assert get_mask_account('12345') == 'Номер счета должен состоять из 20 цифр'
 
 
-
+def test_get_mask_account_empty():
+    assert get_mask_account('') == 'Ведите номер счета'
