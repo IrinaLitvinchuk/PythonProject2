77855c8e7819cc84c021d0d073d29d742fe45440
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-04-28 21:46:40 +0300

    check test_processing.py with linters

--- src/processing.py
+++ src/processing.py
@@ -27,11 +27,40 @@ def filter_by_state(transactions: list, state: str = "EXECUTED") -> str | list[A
 
 def sort_by_date(transactions: list, reverse: bool = True) -> list:
     """Функция сортирует список словарей по дате, по умолчанию - убывание, т.е. сначала новые даты"""
-    # for element in transactions:
-    #     date = element['date']
-    #     if element.get('date') == date:
-    #
+
+    # Сначала собираем все даты из словарей
+    dates = []
+    for element in transactions:
+        current_date = element['date']
+        dates.append(current_date)
+
+        if all(sep in current_date for sep in ["-", ":", ".", "T"]): # для теста на некорректный формат
+            sorted_list = sorted(transactions, key=lambda x: x.get("date"), reverse=reverse)
+        else:
+            raise ValueError("Некорректно указан формат входных данных")
+
+
+    counts = {} # Создаем словарь для подсчета частот дат
+    for date in dates:
+        if date in counts:
+            counts[date] += 1
+        else:
+            counts[date] = 1
+
+    # Проверяем, есть ли повторяющиеся даты
+    duplicates = []
+    for date, count in counts.items():
+        if count > 1:
+            duplicates.append(date)
+
+    # Если есть повторяющиеся даты, выдаем ошибку
+    if duplicates:
+        raise ValueError("Проверьте корректность даты операции: найдены повторяющиеся даты.")
+
+    # Если ошибок нет, выполняем обычную сортировку
     sorted_list = sorted(transactions, key=lambda x: x.get("date"), reverse=reverse)
+
+
     return sorted_list
 
 
