d96456c64f0ed54d7ef301eab308fa74d97d76f8
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-05-02 23:20:42 +0300

    fix test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,7 +1,6 @@
 import pytest
 
 from src.processing import filter_by_state, sort_by_date
-from tests.conftest import transactions
 
 
 def test_filter_by_state(transactions: list) -> None:
@@ -28,10 +27,9 @@ def test_filter_by_state_no_state() -> None:
 
 
 @pytest.mark.parametrize(
-    "transactions, state, expected",
+    "state, expected",
     [
         (
-            transactions,
             "EXECUTED",
             [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -39,7 +37,6 @@ def test_filter_by_state_no_state() -> None:
             ],
         ),
         (
-            transactions,
             "CANCELED",
             [
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -50,18 +47,7 @@ def test_filter_by_state_no_state() -> None:
 )
 def test_filter_by_state_different_state(transactions: list, state: str, expected: list) -> None:
     """Параметризация тестов для различных возможных значений статуса state"""
-    assert (
-        filter_by_state(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            state,
-        )
-        == expected
-    )
+    assert filter_by_state(transactions, state) == expected
 
 
 def test_sort_by_date_descending(transactions: list) -> None:
