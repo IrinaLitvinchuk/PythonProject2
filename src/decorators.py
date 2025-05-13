7d3e472789cb74bc5e3558aef1ae5bacfadf488a
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-05-14 01:16:27 +0300

    check decorators.py with linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,26 +1,31 @@
 from functools import wraps
 from time import time
+from typing import Any, Callable
 
 
-def log(filename=None):
+def log(filename: Any = None) -> None:
     """Декоратор, который логирует работу функции и ее результат в файл (если указан) или выводит в консоль"""
 
-    def decorator(func):
+    def decorator(func: Callable) -> Any:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: tuple, **kwargs: dict) -> Any:
             start_time = time()
             try:
                 result = func(*args, **kwargs)
                 finish_time = time()
-                message = (f"{func.__name__} ok \n"
-                           f"result = {result} \n"
-                           f"Start: {start_time} \n"
-                           f"Finish: {finish_time} \n")
+                message = (
+                    f"{func.__name__} ok \n"
+                    f"result = {result} \n"
+                    f"Start: {start_time} \n"
+                    f"Finish: {finish_time} \n"
+                )
             except Exception as e:
                 finish_time = time()
-                message = (f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} \n"
-                           f"Start: {start_time} \n"
-                           f"Finish: {finish_time} \n")
+                message = (
+                    f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} \n"
+                    f"Start: {start_time} \n"
+                    f"Finish: {finish_time} \n"
+                )
 
                 # Выводим сообщение в файл или в консоль
             if filename is not None:
@@ -32,23 +37,4 @@ def log(filename=None):
 
         return wrapper
 
-    return decorator
-
-
-# Пример использования декоратора
-# @log(filename=None)
-# def my_function(x, y):
-#     return x + y
-#
-#
-# my_function(1, 2)
-# Ожидаемый вывод в лог-файл mylog.txt при успешном выполнении:>>>my_function ok
-# Ожидаемый вывод при ошибке:
-#>>> my_function error: тип ошибки. Inputs: (1, 2), {}
-# Где тип ошибки заменяется на текст ошибки.
-
-# @log(filename="mylog.txt")
-# def my_function(x, y):
-#     return x / y
-#
-# my_function(1, 0)
+    return
