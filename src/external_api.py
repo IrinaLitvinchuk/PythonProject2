7116dfbebb279cc700f1c4b1956d09e949b6bfaa
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-06-04 18:11:16 +0300

    fix external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,5 @@
 import os
 import json
-
 import requests
 from utils import get_transactions
 from dotenv import load_dotenv
@@ -10,55 +9,40 @@ API_KEY = os.getenv('API_KEY')
 
 
 def get_converted_amount(transaction: dict) -> float:
-    """ принимает на вход транзакци и возвращает сумму транзакции в рублях"""
-    to = 'RUB'
-    usd = 'USD'
-    eur = 'EUR'
+    """
+    Принимает на вход транзакцию и возвращает сумму транзакции в рублях.
+    Если валюта RUB, сумма сразу выводится без конвертации.Если USD или EUR,
+    происходит обращение к внешнему API.
+    """
+    to_currency = 'RUB'
     amount = transaction['operationAmount']['amount']
+    currency_code = transaction['operationAmount']['currency']['code']
 
-    if transaction['operationAmount']['currency']['code'] == 'USD':
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={usd}&amount={amount}"
+    if currency_code in ['USD', 'EUR']:
+        try:
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={currency_code}&amount={amount}"
+            headers = {"apikey": API_KEY}
+            response = requests.get(url, headers=headers)
 
-        headers = {
-            "apikey": f"{API_KEY}"
-        }
-        response = requests.request("GET", url, headers=headers)
-        status_code = response.status_code
-        if status_code == 200:
-            try:
-                result = response.json()
-                return result["result"]
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred {e}. Please try again later.")
-                return None
+            if response.status_code != 200:
+                raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
 
-    elif transaction['operationAmount']['currency']['code'] == 'EUR':
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={eur}&amount={amount}"
-        payload = {}
-        headers = {
-            "apikey": API_KEY
-        }
-        response = requests.request("GET", url, headers=headers, data=payload)
-        status_code = response.status_code
-        if status_code == 200:
-            try:
-                result = response.json()
-                return result["result"]
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred {e}. Please try again later.")
-                return None
+            data = response.json()
+            return data['result']
 
-
-    elif transaction['operationAmount']['currency']['code'] == 'RUB':
-        result = transaction['operationAmount']['amount']
-        print(result)
-
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}. Please try again later.")
+            return None
+    else:
+        return amount
 
 
 path = 'data/operations.json'
 transactions_info = get_transactions(path)
 
 for transaction in transactions_info:
-    result = get_converted_amount(transaction)
+    converted_amount = get_converted_amount(transaction)
+    if converted_amount is not None:
+        print(converted_amount)
 
 
