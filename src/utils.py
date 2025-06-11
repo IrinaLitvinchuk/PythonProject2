ed0722865f314eed9f1357921dd2c578934c6fa4
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-06-11 20:45:49 +0300

    checked src with linters

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,22 @@
 import json
-from typing import Dict, List, cast
 import logging
+from typing import Dict, List, cast
 
-utils_logger = logging.getLogger('utils_logger')
-file_handler = logging.FileHandler('../logs/utils.log', mode="w", encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+utils_logger = logging.getLogger("utils_logger")
+file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 utils_logger.addHandler(file_handler)
 utils_logger.setLevel(logging.DEBUG)
 
+
 def get_transactions(path: str) -> List[Dict]:
     """принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
-    utils_logger.info(f'Получен путь до файла {path}')
+    utils_logger.info(f"Получен путь до файла {path}")
     try:
         with open(path, encoding="utf-8") as f:
-            transactions_info = cast(List[Dict], json.load(f))# явно приводим к списку словарей
-        utils_logger.info('Получен список словарей с данными о финансовых транзакциях')
+            transactions_info = cast(List[Dict], json.load(f))  # явно приводим к списку словарей
+        utils_logger.info("Получен список словарей с данными о финансовых транзакциях")
         return transactions_info
     except FileNotFoundError:
         utils_logger.error("Файл не найден")
@@ -27,13 +28,11 @@ def get_transactions(path: str) -> List[Dict]:
         return []
 
 
-path = "data/operations.json" # успешный случай
+path = "data/operations.json"  # успешный случай
 print(get_transactions(path))
 
-path = "data/operation.json" # файл не найден
+path = "data/operation.json"  # файл не найден
 print(get_transactions(path))
 
-path = "data/test_path.json" # не список
+path = "data/test_path.json"  # не список
 print(get_transactions(path))
-
-
